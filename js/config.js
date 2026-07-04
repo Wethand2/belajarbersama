375fbe6179214f1b18bea6116ded78bcdb9086be
Wethand2 <150991662+Wethand2@users.noreply.github.com>
2026-07-04 08:17:53 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // "https://script.google.com/macros/s/AKfycb.../exec"
 // Biarkan kosong ("") untuk mode offline penuh (tanpa sinkronisasi).
 window.CONFIG = {
-  WEBAPP_URL: "",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxEwS6oDr3i18ornCv6ri7SMDJOH4EqE67YxA_1rlNPAn9ObgM03dYpIZyZjhusK1TnmA/exec",
   APP_VERSION: "1.0.0"
 };
